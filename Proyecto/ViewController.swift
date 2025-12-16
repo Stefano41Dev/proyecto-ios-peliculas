b4b1309ad00c53c8960e4924295180114f27f1a9
Misa_ <misa@com.pe>
2025-12-16 10:25:08 -0500

    archivos base

---
+++ Proyecto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto
+//
+//  Created by DESIGN on 16/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
